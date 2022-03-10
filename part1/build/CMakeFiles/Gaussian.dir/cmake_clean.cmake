6acf6957211e07e9b04306b110250f3335387126
Basyno <98152262+Basyno@users.noreply.github.com>
2022-03-11 03:32:02 +0800

    Add files via upload

---
+++ part1/build/CMakeFiles/Gaussian.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gaussian.dir/Gaussianblur.cpp.o"
+  "CMakeFiles/Gaussian.dir/Testbench.cpp.o"
+  "CMakeFiles/Gaussian.dir/main.cpp.o"
+  "Gaussian"
+  "Gaussian.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gaussian.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
